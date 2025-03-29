56eda7b7da848f08d665fb159c630641dcdadae9
Guilherme Freitas <gui2010mg68@gmail.com>
2025-03-28 22:55:25 -0300

    .

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,10 @@
 // databas
 import pkg from 'pg';
-import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
-
 
 const {Pool} = pkg
 
 dotenv.config();
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-
-export default supabase;
-
 
 module.exports = pool;
 
