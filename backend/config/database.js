4c699de6e8b073d4b0e8d28fa73431080b4ba393
Guilherme Freitas <gui2010mg68@gmail.com>
2025-03-28 23:08:46 -0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,15 @@
-// databas
-import pkg from 'pg';
+import { Pool } from 'pg';  // Importação correta do Pool
 import dotenv from 'dotenv';
 
-const {Pool} = pkg
-
+// Carregar variáveis de ambiente
 dotenv.config();
 
-module.exports = pool;
+// Configuração da conexão com o banco de dados
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Exportar a instância de db como exportação padrão
+export default db;
+
 
